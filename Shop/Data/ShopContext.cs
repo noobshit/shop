0154f7cbbfe0cd7281a114bd32ed98793f4f55e2
Andżej <trader1999@o2.pl>
2020-03-02 16:38:10 +0100

    seed db with products

--- Shop/Data/ShopContext.cs
+++ Shop/Data/ShopContext.cs
@@ -17,6 +17,18 @@ namespace Shop.Data
             base.OnModelCreating(modelBuilder);
             modelBuilder.Entity<Product>()
                 .Property(p => p.Price).HasColumnType("decimal(18,2)");
+
+            modelBuilder.Entity<Product>().HasData(
+                new Product { Id=1, ImagePath="sample_1.jpg", Name="Ladder", Price=125.0M},
+                new Product { Id=2, ImagePath="sample_2.jpg", Name="Drill", Price=150M},
+                new Product { Id=3, ImagePath="sample_3.jpg", Name="Grinder", Price=150M},
+                new Product { Id=4, ImagePath="sample_4.jpg", Name="Gloves", Price=3.5M},
+                new Product { Id=5, ImagePath="sample_5.jpg", Name="Thermometer", Price=19.99M},
+                new Product { Id=6, ImagePath="sample_6.jpg", Name="Measure tape", Price=3.5M},
+                new Product { Id=7, ImagePath="sample_7.jpg", Name="WD40", Price=7M},
+                new Product { Id=8, ImagePath="sample_8.jpg", Name="Screwdrivers", Price=12.99M},
+                new Product { Id = 9, ImagePath = "sample_9.jpg", Name = "Lawnmower", Price = 199.99M }
+                );
         }
     }
 }
---
+++ Shop/Migrations/20200302123208_SeedDb.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Shop.Migrations
+{
+    public partial class SeedDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "ImagePath", "Name", "Price" },
+                values: new object[,]
+                {
+                    { 1, "sample_1.jpg", "Ladder", 125.0m },
+                    { 2, "sample_2.jpg", "Drill", 150m },
+                    { 3, "sample_3.jpg", "Grinder", 150m },
+                    { 4, "sample_4.jpg", "Gloves", 3.5m },
+                    { 5, "sample_5.jpg", "Thermometer", 19.99m },
+                    { 6, "sample_6.jpg", "Measure tape", 3.5m },
+                    { 7, "sample_7.jpg", "WD40", 7m },
+                    { 8, "sample_8.jpg", "Screwdrivers", 12.99m },
+                    { 9, "sample_9.jpg", "Lawnmower", 199.99m }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 9);
+        }
+    }
+}
