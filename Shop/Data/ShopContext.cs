e5baa7bdfde5fecd4c52b91b9aae9c9ac99ccff7
Andżej <trader1999@o2.pl>
2020-03-04 15:50:43 +0100

    add more product samples to db

--- Shop/Data/ShopContext.cs
+++ Shop/Data/ShopContext.cs
@@ -31,7 +31,22 @@ namespace Shop.Data
                 new Product { Id=6, ImagePath="sample_6.jpg", Name="Measure tape", Price=3.5M},
                 new Product { Id=7, ImagePath="sample_7.jpg", Name="WD40", Price=7M},
                 new Product { Id=8, ImagePath="sample_8.jpg", Name="Screwdrivers", Price=12.99M},
-                new Product { Id = 9, ImagePath = "sample_9.jpg", Name = "Lawnmower", Price = 199.99M }
+                new Product { Id = 9, ImagePath = "sample_9.jpg", Name = "Lawnmower", Price = 199.99M },
+                new Product { Id = 10, ImagePath = "sample_10.jpg", Name = "Drainpipe", Price = 29M },
+                new Product { Id = 11, ImagePath = "sample_11.jpg", Name = "Window", Price = 400M},
+                new Product { Id = 12, ImagePath = "sample_12.jpg", Name = "Coal", Price = 22.99M},
+                new Product { Id = 13, ImagePath = "sample_13.jpg", Name = "Plywood", Price = 7.50M },
+                new Product { Id = 14, ImagePath = "sample_14.jpg", Name = "Brick", Price = 3.99M },
+                new Product { Id = 15, ImagePath = "sample_15.jpg", Name = "Cement mixer", Price = 400.0M },
+                new Product { Id = 16, ImagePath = "sample_16.jpg", Name = "Barrow", Price = 199.99M },
+                new Product { Id = 17, ImagePath = "sample_17.jpg", Name = "Sink", Price = 169.99M },
+                new Product { Id = 18, ImagePath = "sample_18.jpg", Name = "Board", Price = 35.0M },
+                new Product { Id = 19, ImagePath = "sample_19.jpg", Name = "Lamp", Price = 79.99M },
+                new Product { Id = 20, ImagePath = "sample_20.jpg", Name = "LED Strip", Price = 30.99M },
+                new Product { Id = 21, ImagePath = "sample_21.jpg", Name = "Paper bag", Price = 0.99M },
+                new Product { Id = 22, ImagePath = "sample_22.jpg", Name = "Saw", Price = 39.99M },
+                new Product { Id = 23, ImagePath = "sample_23.jpg", Name = "Knife", Price = 19.99M },
+                new Product { Id = 24, ImagePath = "sample_24.jpg", Name = "Scissors", Price = 9.99M }
                 );
         }
     }
---
+++ Shop/Migrations/20200304144639_IncludeMoreProductSamples.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Shop.Migrations
+{
+    public partial class IncludeMoreProductSamples : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "ImagePath", "Name", "Price" },
+                values: new object[,]
+                {
+                    { 10, "sample_10.jpg", "Drainpipe", 29m },
+                    { 11, "sample_11.jpg", "Window", 400m },
+                    { 12, "sample_12.jpg", "Coal", 22.99m },
+                    { 13, "sample_13.jpg", "Plywood", 7.50m },
+                    { 14, "sample_14.jpg", "Brick", 3.99m },
+                    { 15, "sample_15.jpg", "Cement mixer", 400.0m },
+                    { 16, "sample_16.jpg", "Barrow", 199.99m },
+                    { 17, "sample_17.jpg", "Sink", 169.99m },
+                    { 18, "sample_18.jpg", "Board", 35.0m },
+                    { 19, "sample_19.jpg", "Lamp", 79.99m },
+                    { 20, "sample_20.jpg", "LED Strip", 30.99m },
+                    { 21, "sample_21.jpg", "Paper bag", 0.99m },
+                    { 22, "sample_22.jpg", "Saw", 39.99m },
+                    { 23, "sample_23.jpg", "Knife", 19.99m },
+                    { 24, "sample_24.jpg", "Scissors", 9.99m }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 10);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 11);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 12);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 13);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 14);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 15);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 16);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 17);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 18);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 19);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 20);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 21);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 22);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 23);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 24);
+        }
+    }
+}
